71428c0e15ef4e6e4411f48ac31fac925badf887
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2022-11-03 16:12:22 +0100

    idea and how we could make the ExceptionCatcher

--- SP03/src/ExceptionCatcher.java
+++ SP03/src/ExceptionCatcher.java
@@ -1,3 +1,32 @@
+
 public class ExceptionCatcher
 {
+    public static void wrongInput()
+    {
+        System.out.println("That input is not valid... please try again ");
+    }
+
+    // v maybe use this one below instead of the one above ^
+    public static void indexOutOfBoundsException()
+    {
+        System.out.println("Index out of bounds!");
+    }
+
+    //TODO (idea) add fileNotFoundException to the FileIO class instead of data = null;
+    public static void fileNotFoundException()
+    {
+        System.out.println("File not found");
+    }
+
+    public static void nullPointerExceptiontion()
+    {
+        System.out.println("Null pointer exception");
+    }
+
+
+    public static void ioException()
+    {
+        System.out.println("IO exception");
+    }
+
 }
