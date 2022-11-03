9f038cd7d810c283294c7bd3f41d5e263b07930e
Sebbedeb <zor.seb.0901@gmail.com>
2022-11-03 13:05:34 +0100

    Update StartMenu.java

--- SP03/src/StartMenu.java
+++ SP03/src/StartMenu.java
@@ -1,3 +1,31 @@
 public class StartMenu
 {
+<<<<<<< Updated upstream
+=======
+    static ArrayList<String> registeredUsers = new ArrayList<>();
+    static void setup() {
+        TextUI.welcome();
+        loginScreen();
+    }
+
+    private static void loginScreen() {
+        String input;
+        TextUI.loginOrSignUp();
+        input=FileIO.readUserData();
+        if(input == 1 || input.equals "login"){
+            Setup.login();
+        }
+        else if(input == 2 || input.equals "sign up"){
+            Setup.signUp();
+        }
+        else{
+            ExceptionCatcher.wrongInput();
+        }
+    }
+
+    static ArrayList<String> getRegisteredUsers(){
+        return registeredUsers;
+    }
+
+>>>>>>> Stashed changes
 }
