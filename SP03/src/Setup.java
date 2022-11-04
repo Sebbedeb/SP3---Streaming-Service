4f09ffaf5a84b36a4c43baf8b4aec9378fbecc63
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2022-11-04 12:24:26 +0100

    update to main runs

--- SP03/src/Setup.java
+++ SP03/src/Setup.java
@@ -10,17 +10,9 @@ public class Setup {
         String password;
         ArrayList<String> showsSeen = new ArrayList<>();
         ArrayList<String> favouriteShows = new ArrayList<>();
-        TextUI.signUp();
-        TextUI.newUsername();
-        FileIO.readUserData();
         username = input;
-        TextUI.newPassword();
-        FileIO.readUserData;
         password = input;
         User newUser = new User(username, password, showsSeen, favouriteShows);
-        TextUI.finishSignUp(newUser);
-        FileIO.pressAnything;
-
         IDandPassword.addUser(newUser);
     }
 }
--- SP03/src/StartMenu.java
+++ SP03/src/StartMenu.java
@@ -6,22 +6,12 @@ public class StartMenu
 {
     static ArrayList<String> registeredUsers = new ArrayList<>();
     static void setup() {
-        TextUI.welcome();
+
         loginScreen();
     }
 
     private static void loginScreen() {
-        TextUI.loginOrSignUp();
-        FileIO.readUserData();
-        if(input == 1 || input.equals("login")){
-            Setup.login();
-        }
-        else if(input == 2 || input.equals("sign up")){
-            Setup.signUp();
-        }
-        else{
-            ExceptionCatcher.wrongInput();
-        }
+
     }
 
     static ArrayList<String> getRegisteredUsers(){
