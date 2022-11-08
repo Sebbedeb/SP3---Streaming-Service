850eac31b64081d45aef344ae2e65a7951ecd6d9
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2022-11-08 09:17:12 +0100

    Revert "he"
    This reverts commit 3d3d069ff3fe5986af0b169d9f97de0f3ab3ce05.

--- SP03/src/Setup.java
+++ SP03/src/Setup.java
@@ -1,8 +1,4 @@
 import java.util.ArrayList;
-<<<<<<< HEAD
-=======
-<<<<<<< Updated upstream
->>>>>>> parent of 30b48bd (Big boof)
 
 public class Setup
 {
@@ -27,40 +23,10 @@ public class Setup
         }
 
         User user = new User("temp","temp");
-<<<<<<< HEAD
-=======
-=======
-import java.util.Iterator;
-
-public class Setup {
-    static ArrayList<String> userNames;
-    static ArrayList<String> passWords;
-    TextUI textUI = new TextUI();
-    FileIO fileIO = new FileIO();
-
-    public Setup() {
-    }
-
-    protected User runSetUp() {
-        this.textUI.displayMessage("Welcome to fedFlix! Press 1 to register new user, or press 2 for login");
-        String choice = this.textUI.getUserInput();
-        if (choice.equals("1")) {
-            this.register();
-        } else {
-            this.login();
-        }
-
-        User user = new User("temp", "temp");
->>>>>>> Stashed changes
->>>>>>> parent of 30b48bd (Big boof)
         return user;
     }
 
     protected User login() {
-<<<<<<< HEAD
-=======
-<<<<<<< Updated upstream
->>>>>>> parent of 30b48bd (Big boof)
         String name;
         String pass;
         name = textUI.getUserInput("Please enter your Username");
@@ -112,58 +78,10 @@ public class Setup {
             textUI.displayMessage("Please only use letters and numbers");
             register();
         }
-<<<<<<< HEAD
-=======
-=======
-        String name = this.textUI.getUserInput("Please enter your Username");
-        String pass = this.textUI.getUserInput("Please enter your Password");
-        ArrayList<User> users = this.createUsers(userNames, passWords);
-        Iterator var4 = users.iterator();
-
-        while(var4.hasNext()) {
-            User i = (User)var4.next();
-            if (name == i.getUsername() && pass == i.getPassword()) {
-                return i;
-            }
-
-            this.textUI.displayMessage("Username or Password does not exist. Please try again.");
-            this.login();
-        }
-
->>>>>>> Stashed changes
->>>>>>> parent of 30b48bd (Big boof)
         return null;
     }
 
 
-<<<<<<< HEAD
 
 
-=======
-        for(int i = 0; i < userNames.size() - 1; ++i) {
-            String name = (String)userNames.get(i);
-            String pass = (String)passWords.get(i);
-            User user = new User(name, pass);
-            users.add(user);
-        }
-
-        return users;
-    }
-
-<<<<<<< Updated upstream
-
-
-=======
-    private User register() {
-        this.textUI.displayMessage("Register new user:");
-        String name = this.textUI.getUserInput("Username: ");
-        userNames.add(name);
-        String pass = this.textUI.getUserInput("Password: ");
-        passWords.add(pass);
-        User user = new User(name, pass);
-        this.fileIO.writeUserData(user);
-        return user;
-    }
->>>>>>> Stashed changes
->>>>>>> parent of 30b48bd (Big boof)
 }
